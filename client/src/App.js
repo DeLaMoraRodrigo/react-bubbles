e8abb2ca610c9e4d3b8410ab86b546efc23978bd
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-04-24 08:34:01 -0700

    Brought in axiosWithAuth and ProtectedRoute files, used ProtectedRoute to route to BubblePage. Created Login form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
+import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <nav>
+          <NavLink to="/">
+            <h3>HOME</h3>
+          </NavLink>
+          <NavLink to="/login">
+            <h3>LOG IN</h3>
+          </NavLink>
+          <NavLink to="/bubble-page">
+            <h3>BUBBLE PAGE</h3>
+          </NavLink>
+        </nav>
+
+        <Route exact path="/login" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <ProtectedRoute exact path="/bubble-page" component={BubblePage} />
       </div>
     </Router>
   );
